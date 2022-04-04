e89ff9c1f19330984c80e5c12d565701cba182f2
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-04 15:19:27 +0900

    Setting next.js

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  // Override any other rules you want
+};
---
+++ components/Staking/index.tsx
@@ -0,0 +1,5 @@
+const Staking = () => {
+  return <div>test</div>;
+};
+
+export default Staking;
