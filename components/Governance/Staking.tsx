c57d4a40bd04a491652bdfea5ae62c741aff76d1
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-12 17:54:13 +0900

    Modify useTranslation

--- components/Governance/Staking.tsx
+++ components/Governance/Staking.tsx
@@ -1,7 +1,11 @@
 import ModalType from 'enums/ModalType';
 import { Dispatch, SetStateAction, useMemo } from 'react';
 import styles from './Governance.module.scss';
-import { formatComma, formatSixFracionDigit } from 'utils/formatters';
+import {
+  formatComma,
+  formatSixFracionDigit,
+  toCompactForBignumber,
+} from 'utils/formatters';
 import useStakedInfo from 'hooks/useStakedInfo';
 import { formatEther } from 'ethers/lib/utils';
 import CountUp from 'react-countup';
@@ -9,6 +13,11 @@ import { BigNumber } from 'ethers';
 import Arrow from './Arrow';
 import { useWeb3React } from '@web3-react/core';
 import { useRouter } from 'next/router';
+import useTotalStakedBalance from 'hooks/useTotalStakedBalance';
+import Skeleton from 'react-loading-skeleton';
+import ChainType from 'enums/ChainType';
+import { Trans, useTranslation } from 'react-i18next';
+import LanguageType from 'enums/LanguageType';
 
 type Props = {
   setModalType: Dispatch<SetStateAction<ModalType | undefined>>;
@@ -17,27 +26,37 @@ type Props = {
     before: BigNumber;
     after: BigNumber;
   };
+  currentChain: ChainType;
+  setCurrentChain: Dispatch<SetStateAction<ChainType>>;
 };
 
 const Staking = (props: Props) => {
-  const { setModalType, setModalVisible, reward } = props;
+  const {
+    setModalType,
+    setModalVisible,
+    reward,
+    currentChain,
+    setCurrentChain,
+  } = props;
   const { account } = useWeb3React();
   const router = useRouter();
   const userStakedInfo = useStakedInfo();
+  const { totalBalance, isLoading } = useTotalStakedBalance();
+  const { t, i18n } = useTranslation();
 
   const stakingInfo = useMemo(() => {
     return [
       {
-        name: '스테이킹 수량',
+        name: t('governance.section_third.7'),
         value: `${account ? formatComma(userStakedInfo.userPrincipal) : 0}`,
-        btnType: '스테이킹 / 언스테이킹',
+        btnType: t('governance.section_third.9'),
         onClick: () => {
           setModalType(account ? ModalType.Staking : ModalType.NoAccount);
           setModalVisible();
         },
       },
       {
-        name: '보상 수량',
+        name: t('governance.section_third.8'),
         value: (
           <>
             <CountUp
@@ -52,7 +71,7 @@ const Staking = (props: Props) => {
             />
           </>
         ),
-        btnType: 'Claim',
+        btnType: t('governance.section_third.10'),
         onClick: () => {
           setModalType(account ? ModalType.Claim : ModalType.NoAccount);
           setModalVisible();
@@ -75,76 +94,106 @@ const Staking = (props: Props) => {
             02
           </div>
           <p className={styles.governance_staking_content_description_second}>
-            Get sEL tokens by staking EL tokens
+            {t('governance.section_third.0')}
           </p>
           {/* <div
             className={
               styles.governance_staking_content_description_wrapper_third
             }> */}
           <p className={styles.governance_staking_content_description_third}>
-            의사결정과정에참여하기위해서는투표권(staked EL, sEL)이있어야합니다.{' '}
-            <br /> EL 스테이킹앱에EL 토큰을스테이킹함으로써투표권을얻을수있으며,
-            <br />
-            보유한수량만큼투표권을행사할수있습니다
+            <Trans>{t('governance.section_third.1')}</Trans>
           </p>
           <p className={styles.governance_staking_content_description_third}>
-            스테이킹기간은 <strong>ELYSIA 2.0</strong> 오픈전까지이며,
-            언제든지언스테이킹이가능 합니다.
+            <Trans> {t('governance.section_third.2')}</Trans>
           </p>
           {/* </div> */}
-          <div className={styles.governance_staking_content_button}>
-            스테이킹 가이드
+          <div
+            className={styles.governance_staking_content_button}
+            style={{
+              fontSize:
+                i18n.language === LanguageType.EN ? '0.94rem' : undefined,
+            }}>
+            {t('governance.section_third.3')}
             <Arrow />
           </div>
         </div>
         <div className={styles.governance_staking_info}>
           <div className={styles.governance_staking_total}>
-            <div>xx.xx M</div>
+            <div>
+              {currentChain === ChainType.Ethereum ? (
+                isLoading ? (
+                  <Skeleton width={130} height={35} />
+                ) : (
+                  toCompactForBignumber(totalBalance)
+                )
+              ) : (
+                '-'
+              )}
+            </div>
             <div className={styles.governance_staking_total_name}>
-              총 스테이킹량
+              {t('governance.section_third.4')}
             </div>
           </div>
           <div className={styles.governance_staking_apr}>
-            <div>xx.xx %</div>
+            <div> {currentChain === ChainType.Ethereum ? 'xx.xx %' : '-'}</div>
             <div className={styles.governance_staking_total_name}>APR</div>
           </div>
         </div>
       </div>
       <div className={styles.staking_wrapper}>
         <div className={styles.staking_container}>
-          <div className={styles.staking_container_header}>EL Staking APP</div>
+          <div className={styles.staking_container_header}>
+            {t('governance.section_third.5')}
+          </div>
           <div className={styles.staking_header}>
             <div className={styles.staking_select_mainnet}>
-              <div>Ethereum</div>
-              <div>BSC</div>
+              <div
+                onClick={() => setCurrentChain(ChainType.Ethereum)}
+                style={{
+                  color:
+                    currentChain === ChainType.Ethereum ? '#333333' : '#888888',
+                }}>
+                Ethereum
+              </div>
+              <div
+                onClick={() => setCurrentChain(ChainType.BSC)}
+                style={{
+                  color: currentChain === ChainType.BSC ? '#333333' : '#888888',
+                }}>
+                BSC
+              </div>
             </div>
             <div
               className={styles.staking_prev}
               onClick={() => router.push(`/ko/Governance/prevstaking`)}>
-              이전 스테이킹 프로그램 &gt;
+              {t('governance.section_third.6')} &gt;
             </div>
           </div>
           <div className={styles.staking_content}>
-            {stakingInfo.map((info, idx) => (
-              <div key={`info_${idx}`} className={styles.staking_content_box}>
-                <div className={styles.staking_content_header}>
-                  <div>{info.name}</div>
-                  <div
-                    onClick={() => {
-                      info.onClick();
-                    }}>
-                    {' '}
-                    {info.btnType}
+            {currentChain === ChainType.Ethereum ? (
+              stakingInfo.map((info, idx) => (
+                <div key={`info_${idx}`} className={styles.staking_content_box}>
+                  <div className={styles.staking_content_header}>
+                    <div>{info.name}</div>
+                    <div
+                      onClick={() => {
+                        info.onClick();
+                      }}>
+                      {' '}
+                      {info.btnType}
+                    </div>
+                  </div>
+                  <div>
+                    <div className={styles.staking_content_box_amount}>
+                      <span>{info.value}</span>
+                      <span>EL</span>
+                    </div>
                   </div>
                 </div>
-                <div>
-                  <div className={styles.staking_content_box_amount}>
-                    <span>{info.value}</span>
-                    <span>EL</span>
-                  </div>
-                </div>
-              </div>
-            ))}
+              ))
+            ) : (
+              <div className={styles.staking_comming_soon}>Comming soon!</div>
+            )}
           </div>
         </div>
       </div>
