2cfbd400962b48f10ec81d0ce31eae715ed9de7a
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-12 17:36:30 +0900

    Add props

--- components/PrevStaking/StakingItem.tsx
+++ components/PrevStaking/StakingItem.tsx
@@ -1,6 +1,11 @@
 import { useWeb3React } from '@web3-react/core';
-import { BigNumber, utils } from 'ethers';
+import LanguageType from 'enums/LanguageType';
+import ModalType from 'enums/ModalType';
+import { BigNumber, constants, utils } from 'ethers';
+import usePrevStaking from 'hooks/usePrevStaing';
 import moment from 'moment';
+import { Dispatch, SetStateAction } from 'react';
+import { useTranslation } from 'react-i18next';
 import Skeleton from 'react-loading-skeleton';
 import { formatComma } from 'utils/formatters';
 import { IStakingPoolRound } from 'utils/stakingRoundDate';
@@ -14,6 +19,10 @@ import {
 } from './styles';
 
 type Props = {
+  setModalType?: Dispatch<SetStateAction<ModalType | undefined>>;
+  setModalVisible?: () => void;
+  setAmount?: Dispatch<SetStateAction<BigNumber>>;
+  setRound?: () => void;
   date?: IStakingPoolRound;
   round?: number;
   userInfo?: {
@@ -25,7 +34,17 @@ type Props = {
 
 const StakingItem = (props: Props) => {
   const { account } = useWeb3React();
-  const { date, round, userInfo, isLoading } = props;
+  const { t, i18n } = useTranslation();
+  const {
+    date,
+    round,
+    userInfo,
+    isLoading,
+    setModalType,
+    setModalVisible,
+    setAmount,
+    setRound,
+  } = props;
 
   return (
     <>
@@ -36,7 +55,13 @@ const StakingItem = (props: Props) => {
               <Skeleton height={141} width={208} />
             ) : (
               <>
-                <div>{round ? round + '차' : '1차 ~ 7차'}</div>
+                <div>
+                  {round
+                    ? i18n.language === LanguageType.EN
+                      ? t(`governance.prev_staking.5.${round}`)
+                      : t('governance.prev_staking.5', { round })
+                    : t('governance.prev_staking.2')}
+                </div>
                 <div>
                   {moment(date?.startedAt || '2021.07.27').format('YYYY.MM.DD')}{' '}
                   ~ <br />
@@ -53,18 +78,52 @@ const StakingItem = (props: Props) => {
           {userInfo ? (
             <>
               <StakingInfoByRound>
-                <div>스테이킹 수량</div>
+                <div>{t('governance.prev_staking.6')}</div>
                 <div>
                   {formatComma(userInfo.userPrincipal)} <span>EL</span>
                 </div>
-                <div>언스테이킹</div>
+                <div
+                  onClick={() => {
+                    if (userInfo.userPrincipal.lte(constants.Zero)) {
+                      return;
+                    }
+                    setModalVisible?.();
+                    setModalType?.(ModalType.PrevUnstake);
+                    setRound?.();
+                    setAmount?.(userInfo.userPrincipal);
+                  }}
+                  style={{
+                    backgroundColor: userInfo.userPrincipal.lte(constants.Zero)
+                      ? '#f0f0f1'
+                      : '#ffffff',
+                    boxShadow: '0px 1px 6px rgb(0 0 0 / 16%)',
+                  }}>
+                  {t('governance.prev_staking.8')}
+                </div>
               </StakingInfoByRound>
               <StakingInfoByRound>
-                <div>보상 수량</div>
+                <div>{t('governance.prev_staking.7')}</div>
                 <div>
                   {formatComma(userInfo.userReward)} <span>EL</span>
                 </div>
-                <div>수령하기</div>
+                <div
+                  onClick={() => {
+                    if (userInfo.userReward.lte(constants.Zero)) {
+                      return;
+                    }
+                    setModalVisible?.();
+                    setRound?.();
+                    setAmount?.(userInfo.userReward);
+                    setModalType?.(ModalType.PrevReward);
+                  }}
+                  style={{
+                    backgroundColor: userInfo.userReward.lte(constants.Zero)
+                      ? '#f0f0f1'
+                      : '#ffffff',
+                    boxShadow: '0px 1px 6px rgb(0 0 0 / 16%)',
+                  }}>
+                  {t('governance.prev_staking.9')}
+                </div>
               </StakingInfoByRound>
             </>
           ) : isLoading && account ? (
@@ -74,8 +133,8 @@ const StakingItem = (props: Props) => {
           ) : (
             <WalletText>
               {account
-                ? '스테이킹한 EL 토큰 수량이 없습니다.'
-                : '지갑 연결이 필요합니다.'}
+                ? t('governance.prev_staking.4')
+                : t('governance.prev_staking.3')}
             </WalletText>
           )}
         </StakingInfoWrapper>
