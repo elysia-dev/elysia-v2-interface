821039f751c1f2e0db450ad2ed9a43955d4b4235
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-12 17:52:10 +0900

    Modify modal input

--- components/Modals/StakingBody.tsx
+++ components/Modals/StakingBody.tsx
@@ -1,7 +1,7 @@
 import { BigNumber, utils } from 'ethers';
-import { Dispatch, SetStateAction } from 'react';
-import IncreateAllowanceModal from './IncreateAllowanceModal';
-import LoadingIndicator from './LoadingIndicator';
+import { Dispatch, SetStateAction, useEffect, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { formatComma } from 'utils/formatters';
 import styles from './Modal.module.scss';
 
 type Props = {
@@ -9,44 +9,75 @@ type Props = {
     header: string;
     walletAmount: string;
     max: () => void;
-    value: number;
-    setValue: Dispatch<SetStateAction<number>>;
-    amount: string;
+    value: string;
+    setValue: Dispatch<SetStateAction<string>>;
+    amount: BigNumber;
     type: string;
-    sendTx: (amount: BigNumber) => void;
+    sendTx: (amount: BigNumber, round?: number) => void;
+    round?: number;
   };
   setTransactionWait: Dispatch<SetStateAction<boolean>>;
 };
 
 const StakingBody = (props: Props) => {
   const { stakingInfo, setTransactionWait } = props;
+  const { t } = useTranslation();
+  const isDisabledBtn = useMemo(() => {
+    return (
+      parseFloat(utils.formatUnits(stakingInfo.amount)) <
+        Number(stakingInfo.value) || Number(stakingInfo.value) === 0
+    );
+  }, [stakingInfo.value, stakingInfo.amount]);
 
   return (
     <>
       <>
         <div className={styles.amount_input}>
-          <div onClick={() => stakingInfo.max()}>최대</div>
+          <div onClick={() => stakingInfo.max()}>{t('modal.amount_max')}</div>
           <input
+            className="amount"
+            type="number"
             placeholder="0"
             value={stakingInfo.value}
-            onChange={(e: any) => stakingInfo.setValue(e.target.value)}
+            onChange={(e: any) => {
+              if (e.target.value[0] === '.') {
+                stakingInfo.setValue(0.0 + e.target.value);
+                return;
+              }
+              stakingInfo.setValue(e.target.value);
+            }}
           />
         </div>
         <div className={styles.balance_wrapper}>
           <div className={styles.balance_header}>{stakingInfo.header}</div>
           <div className={styles.balance_content}>
             <div>{stakingInfo.walletAmount}</div>
-            <div>{stakingInfo.amount}</div>
+            <div>{formatComma(stakingInfo.amount)} EL</div>
           </div>
         </div>
         <div className="wallet_select_modal__content__line" />
         <div
           className={styles.modal_button}
           onClick={() => {
+            if (isDisabledBtn) {
+              return;
+            }
+
             setTransactionWait(true);
-            stakingInfo.sendTx(utils.parseEther(String(stakingInfo.value)));
+            stakingInfo.sendTx(
+              utils.parseEther(String(stakingInfo.value)),
+              stakingInfo.round,
+            );
+          }}
+          style={{
+            backgroundColor: isDisabledBtn ? '#f0f0f1' : '#3679b5',
           }}>
-          <p>{stakingInfo.type}</p>
+          <p
+            style={{
+              color: isDisabledBtn ? '#888888' : '#ffffff',
+            }}>
+            {isDisabledBtn ? t('modal.button.0') : stakingInfo.type}
+          </p>
         </div>
       </>
     </>
