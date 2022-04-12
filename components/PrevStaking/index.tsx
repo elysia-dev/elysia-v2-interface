5934acaee2858228ca753e13827aa9ad0b239eab
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-12 17:37:08 +0900

    Add prevUnStakingModal

--- components/PrevStaking/index.tsx
+++ components/PrevStaking/index.tsx
@@ -1,44 +1,79 @@
-import { useWeb3React } from '@web3-react/core';
-import usePrevStaking from 'hooks/usePrevStaking';
 import stakingRoundDate from 'utils/stakingRoundDate';
+import { useContext, useEffect, useState } from 'react';
+import ModalType from 'enums/ModalType';
 import StakingItem from './StakingItem';
 import { HeaderWrapper, PrevStakingWrapper, StakingSection } from './styles';
+import Modal from 'components/Modals';
+import usePrevStakingInfo from 'hooks/usePrevStakingInfo';
+import { constants } from 'ethers';
+import RecentActivityType from 'enums/RecentActivityType';
+import TxContext from 'contexts/TxContext';
+import { Trans, useTranslation } from 'react-i18next';
 
 const PrevStaking = () => {
-  const { userInfo, isLoading } = usePrevStaking();
-  const { account } = useWeb3React();
+  const { userInfo, isLoading } = usePrevStakingInfo();
+  const { t } = useTranslation();
+  const { txType } = useContext(TxContext);
+  const [modal, setModalType] = useState<ModalType>();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [round, setRound] = useState(0);
+  const [amount, setAmount] = useState(constants.Zero);
+
+  useEffect(() => {
+    if (
+      txType === RecentActivityType.ELStake ||
+      txType === RecentActivityType.ELStakingWithdraw ||
+      txType === RecentActivityType.ELClaim
+    ) {
+      setModalType(ModalType.ConfirmEnded);
+    }
+  }, [txType]);
 
   return (
-    <PrevStakingWrapper>
-      <HeaderWrapper>
-        <div>이전 스테이킹 프로그램</div>
+    <>
+      <Modal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        modalType={modal}
+        setModalType={() => setModalType(ModalType.Connect)}
+        prevAmount={amount}
+        round={round}
+      />
+      <PrevStakingWrapper>
+        <HeaderWrapper>
+          <div>{t('governance.prev_staking.0')}</div>
+          <div>
+            <Trans>{t('governance.prev_staking.1')}</Trans>
+          </div>
+        </HeaderWrapper>
         <div>
-          이전 EL 스테이킹 프로그램은 <span>2021.07.27 ~ 2022.04.17 KST</span>{' '}
-          까지 7회차 동안 진행했습니다.
-          <br /> 이 기간동안 스테이킹한 물량이 남아있는 사용자만 확인할 수
-          있습니다.
-          <br /> 언스테이킹 및 보상 수령을 원하는 사용자는 지갑 연결을 해주세요.
+          <StakingSection>
+            {userInfo.length > 1 ? (
+              stakingRoundDate.map((date, idx) => {
+                return (
+                  <StakingItem
+                    key={`staking_item_${idx}`}
+                    date={date}
+                    round={idx + 1}
+                    userInfo={userInfo[idx]}
+                    setModalType={setModalType}
+                    setModalVisible={() => setModalVisible(true)}
+                    setAmount={setAmount}
+                    setRound={() => setRound(idx + 1)}
+                  />
+                );
+              })
+            ) : (
+              <StakingItem
+                isLoading={isLoading}
+                setModalType={setModalType}
+                setModalVisible={() => {}}
+              />
+            )}
+          </StakingSection>
         </div>
-      </HeaderWrapper>
-      <div>
-        <StakingSection>
-          {userInfo.length > 1 ? (
-            stakingRoundDate.map((date, idx) => {
-              return (
-                <StakingItem
-                  key={`staking_item_${idx}`}
-                  date={date}
-                  round={idx + 1}
-                  userInfo={userInfo[idx]}
-                />
-              );
-            })
-          ) : (
-            <StakingItem isLoading={isLoading} />
-          )}
-        </StakingSection>
-      </div>
-    </PrevStakingWrapper>
+      </PrevStakingWrapper>
+    </>
   );
 };
 
