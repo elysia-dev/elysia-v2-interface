9679178a02c33019c39917e6db0377bfef5c33c3
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-13 20:47:34 +0900

    Add React fragment

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -47,13 +47,15 @@ const Navigation = () => {
       <div className={styles.navigation}>
         <div className={styles.navigation_wrapper}>
           <div className={styles.navigation_logo}>
-            <Link href={`/${router.query.lng}/Governance`} passHref>
-              <Image
-                src={ElysiaLogo}
-                alt={'ElysiaLogo'}
-                width={isMobile ? 80 : 139}
-                height={isMobile ? 18 : 31}
-              />
+            <Link href={`/${router.query.lng}/Governance`}>
+              <>
+                <Image
+                  src={ElysiaLogo}
+                  alt={'ElysiaLogo'}
+                  width={isMobile ? 80 : 139}
+                  height={isMobile ? 18 : 31}
+                />
+              </>
             </Link>
           </div>
           <div>
