e498b5a6fbe2fbe017548c23b34f6c8b35ea1715
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-13 18:57:02 +0900

    Add v1 staking pool

---
+++ hooks/useV1StakingPool.ts
@@ -0,0 +1,25 @@
+import { useWeb3React } from '@web3-react/core';
+import { useMemo } from 'react';
+import envs from 'core/envs';
+
+import {
+  StakingPool,
+  StakingPool__factory,
+} from '@elysia-dev/contract-typechain';
+
+const useV1StakingPool = (): {
+  contract: StakingPool | undefined;
+} => {
+  const { library } = useWeb3React();
+  const contract = useMemo(() => {
+    if (!library) return;
+    return StakingPool__factory.connect(
+      envs.staking.elStakingPoolAddress,
+      library.getSigner(),
+    );
+  }, [library]);
+
+  return { contract };
+};
+
+export default useV1StakingPool;
