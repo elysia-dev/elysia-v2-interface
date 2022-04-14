4ff6c6d2622ef336180b4e7b2c19fd526dd6d420
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-14 16:49:44 +0900

    Add apr

--- hooks/useTotalStakedBalance.ts
+++ hooks/useTotalStakedBalance.ts
@@ -1,21 +1,33 @@
 import { ERC20__factory } from '@elysia-dev/contract-typechain';
 import { useWeb3React } from '@web3-react/core';
+import { pricesFetcher } from 'clients/Coingecko';
 import envs from 'core/envs';
-import { constants, providers, utils } from 'ethers';
+import { BigNumber, constants, providers, utils } from 'ethers';
+import priceMiddleware from 'middleware/priceMiddleware';
 import { useEffect, useState } from 'react';
+import useSWR from 'swr';
 import calcAPR from 'utils/calcAPR';
+import { toPercent } from 'utils/formatters';
 import useERC20 from './useERC20';
 import useStakingPool from './useStakingPool';
 
 const useTotalStakedBalance = () => {
   const { account } = useWeb3React();
   const [totalBalance, setTotalBalance] = useState(constants.Zero);
+  const [apr, setApr] = useState('-');
   const [isLoading, setIsLoading] = useState(true);
   const contract = useERC20(envs.token.elAddress);
   const { contract: v2Contract } = useStakingPool();
+  const { data } = useSWR(
+    envs.externalApiEndpoint.coingackoURL,
+    pricesFetcher,
+    {
+      use: [priceMiddleware],
+    },
+  );
 
   useEffect(() => {
-    if (!contract || !v2Contract) return;
+    if (!contract || !v2Contract || !data) return;
     (async () => {
       try {
         const v1Balance = await contract.balanceOf(
@@ -23,18 +35,26 @@ const useTotalStakedBalance = () => {
         );
         const v2Balance = await v2Contract.getPoolData();
 
-        // calcAPR()
-
-        setTotalBalance(v2Balance.totalPrincipal.add(v1Balance));
+        setApr(
+          toPercent(
+            calcAPR(
+              v2Balance.totalPrincipal,
+              data?.elPrice,
+              utils.parseEther('330731.57142857'),
+              data?.elPrice,
+            ),
+          ),
+        ),
+          setTotalBalance(v2Balance.totalPrincipal.add(v1Balance));
         setIsLoading(false);
       } catch (error) {
         console.log(error);
         setIsLoading(false);
       }
     })();
-  }, [contract, account, v2Contract]);
+  }, [contract, account, v2Contract, data]);
 
-  return { totalBalance, isLoading };
+  return { totalBalance, apr, isLoading };
 };
 
 export default useTotalStakedBalance;
