6516ea3e32ca16b06d1dffda2c196b9287a423b1
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-14 16:16:17 +0900

    Modify totalBalance

--- hooks/useTotalStakedBalance.ts
+++ hooks/useTotalStakedBalance.ts
@@ -1,33 +1,38 @@
 import { ERC20__factory } from '@elysia-dev/contract-typechain';
 import { useWeb3React } from '@web3-react/core';
 import envs from 'core/envs';
-import { constants, providers } from 'ethers';
+import { constants, providers, utils } from 'ethers';
 import { useEffect, useState } from 'react';
+import calcAPR from 'utils/calcAPR';
 import useERC20 from './useERC20';
+import useStakingPool from './useStakingPool';
 
 const useTotalStakedBalance = () => {
   const { account } = useWeb3React();
   const [totalBalance, setTotalBalance] = useState(constants.Zero);
   const [isLoading, setIsLoading] = useState(true);
   const contract = useERC20(envs.token.elAddress);
+  const { contract: v2Contract } = useStakingPool();
 
   useEffect(() => {
-    if (!contract) return;
+    if (!contract || !v2Contract) return;
     (async () => {
       try {
         const v1Balance = await contract.balanceOf(
           envs.staking.elStakingPoolAddress,
         );
-        const v2Balance = await contract.balanceOf(
-          envs.staking.elfyV2StakingPoolAddress,
-        );
-        setTotalBalance(v2Balance.add(v1Balance));
+        const v2Balance = await v2Contract.getPoolData();
+
+        // calcAPR()
+
+        setTotalBalance(v2Balance.totalPrincipal.add(v1Balance));
         setIsLoading(false);
       } catch (error) {
+        console.log(error);
         setIsLoading(false);
       }
     })();
-  }, [contract, account]);
+  }, [contract, account, v2Contract]);
 
   return { totalBalance, isLoading };
 };
