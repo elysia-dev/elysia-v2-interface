43521e7badad550f6d9b34991a75d533659fa6c7
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-15 15:06:30 +0900

    Remove import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import Navigation from 'components/Navigation';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import getLocalLanguage from 'utils/getLocalLanguage';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   const router = useRouter();
