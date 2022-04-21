c7469dd1e52da918b715b7ceaeb0e3bd2aecebce
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-21 19:55:23 +0900

    Add Main portfolio component

---
+++ components/Main/PortFolio.tsx
@@ -0,0 +1,42 @@
+import { MainPortFolioWrapper } from './styles';
+
+const PortFolio = () => {
+  return (
+    <MainPortFolioWrapper>
+      <div>
+        <div>ELYSIA PORTFOLIO</div>
+        <div>Tokenized real world asset.</div>
+        엘리시아가 현재까지 진행한 부동산 포트폴리오.
+      </div>
+      <div>
+        <div>
+          <div>이미지</div>
+          <div>
+            <div>부동산</div>
+            <div>아이콘</div>
+            <div>부동산 value</div>
+          </div>
+        </div>
+        <div>
+          <div>이미지</div>
+          <div>
+            <div>부동산</div>
+            <div>아이콘</div>
+            <div>부동산 value</div>
+          </div>
+        </div>
+        <div>
+          <div>이미지</div>
+          <div>
+            <div>부동산</div>
+            <div>아이콘</div>
+            <div>부동산 value</div>
+          </div>
+        </div>
+      </div>
+      <div>&gt;&gt; More portfolio</div>
+    </MainPortFolioWrapper>
+  );
+};
+
+export default PortFolio;
