c10e299c54001813e7c372279e5c9335fb9c6638
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-21 11:12:42 +0900

    Add useSWR

--- hooks/useERC20Info.ts
+++ hooks/useERC20Info.ts
@@ -1,9 +1,11 @@
 import { ERC20 } from '@elysia-dev/elyfi-v1-sdk';
 import { useWeb3React } from '@web3-react/core';
+import { tokenInfoFetcher } from 'clients/StakingFetcher';
 import TxContext from 'contexts/TxContext';
 import TxStatus from 'enums/TxStatus';
 import { BigNumber, constants } from 'ethers';
 import { useCallback, useContext, useEffect, useState } from 'react';
+import useSWR from 'swr';
 import useERC20 from './useERC20';
 
 interface IERC20Info {
@@ -35,43 +37,46 @@ const useERC20Info = (
     error: null,
   });
 
-  const load = useCallback(
-    async (account: string) => {
-      try {
-        setState({
-          ...state,
-          loading: true,
-        });
-        const balance = await contract.balanceOf(account);
-        const allowance = await contract.allowance(account, targetAddress);
-        setState({
-          ...state,
-          allowance,
-          balance,
-          loading: false,
-        });
-      } catch (error) {
-        setState({
-          ...state,
-          allowance: constants.Zero,
-          balance: constants.Zero,
-          loading: false,
-          error: null,
-        });
-      }
-    },
-    [contract, targetAddress, state],
-  );
+  const { data, mutate } = useSWR([contract, account, targetAddress, 'erc20'], {
+    fetcher: tokenInfoFetcher(),
+  });
+
+  const load = useCallback(async () => {
+    try {
+      if (!data) return;
+
+      setState({
+        ...state,
+        loading: true,
+      });
+      const balance = data[0];
+      const allowance = data[1];
+      setState({
+        ...state,
+        allowance,
+        balance,
+        loading: false,
+      });
+    } catch (error) {
+      setState({
+        ...state,
+        allowance: constants.Zero,
+        balance: constants.Zero,
+        loading: false,
+        error: null,
+      });
+    }
+  }, [contract, state, data]);
 
   useEffect(() => {
     if (txStatus === TxStatus.CONFIRM && account) {
-      load(account);
+      mutate();
     }
-  }, [txStatus, account]);
+  }, [txStatus, account, data]);
 
   useEffect(() => {
     if (account) {
-      load(account);
+      load();
     } else {
       setState({
         ...state,
@@ -81,12 +86,12 @@ const useERC20Info = (
         error: null,
       });
     }
-  }, [account, targetAddress, visible]);
+  }, [account, visible, data]);
 
   return {
     ...state,
     refetch: () => {
-      account && load(account);
+      account && load();
     },
     contract,
   };
