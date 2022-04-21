3b33c55bc5501227e06d6738ab9f04e93615cccd
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-21 19:54:34 +0900

    Add Main top

---
+++ components/Main/Top.tsx
@@ -0,0 +1,61 @@
+import { MainTopWrapper } from './styles';
+
+const Top = () => {
+  return (
+    <MainTopWrapper>
+      <div>
+        <div>
+          <div>ELYSIA PROTOCOL</div>
+          <div>Real world asset tokenization DAO.</div>
+          엘리시아는 부동산과 같은 실물자산을 토큰화하고,
+          <br /> 이를 탈중앙화조직 DAO를 이용해 유동화할 수 있는 서비스를
+          제공합니다.
+          <br /> 사용자는 이 토큰을 토대로 디파이 등 다양한 블록체인 생태계에서
+          활동할 수 있습니다.
+        </div>
+        <div>
+          <div>Twitter icon1</div>
+          <div>Telegram icon1</div>
+          <div>Discord icon12</div>
+          <div>Github icon 13</div>
+        </div>
+        <div>
+          <div>
+            <div>icon</div>
+            <div>
+              <span>20+</span>
+              <br />
+              Changed Real Estate
+            </div>
+          </div>
+          <div>
+            <div>icon</div>
+            <div>
+              <span>2,000+</span>
+              <br />
+              Community Delegates
+            </div>
+          </div>
+          <div>
+            <div>icon</div>
+            <div>
+              <span>$ 5M+</span>
+              <br />
+              Total Value Locked
+            </div>
+          </div>
+          <div>
+            <div>icon</div>
+            <div>
+              <span>3000+</span>
+              <br />
+              Github commit
+            </div>
+          </div>
+        </div>
+      </div>
+    </MainTopWrapper>
+  );
+};
+
+export default Top;
