6e76496bd8f72d545cdbe15c79bd3059a2eab719
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-21 19:55:13 +0900

    Add Main section component

---
+++ components/Main/Section.tsx
@@ -0,0 +1,118 @@
+import { MainSectionWrapper } from './styles';
+
+const Section = () => {
+  return (
+    <MainSectionWrapper>
+      <div>
+        <div>
+          <div>
+            <span>EL Bridge (On-boarding) -&gt;</span>
+            <br />
+            <span>Real world asset into digital NFT.</span>
+            <br />
+            실제 부동산을 NFT로 만드는 과정.
+          </div>
+          <p>
+            사용자들은 DAO를 통해 실제 부동산이 NFT로 만들어지는 과정에
+            참여하고,
+            <br />
+            제대로 변환이 이루어지는지 감시할 수 있습니다.
+          </p>
+        </div>
+        <div>이미지</div>
+      </div>
+      <div>
+        <div>이미지</div>
+        <div>
+          <div>
+            <span>&lt;- EL Bridge (On-boarding)</span>
+            <br />
+            <span>Real world asset into digital NFT.</span>
+            <br />
+            실제 부동산을 NFT로 만드는 과정.
+          </div>
+          <p>
+            사용자들은 DAO를 통해 실제 부동산이 NFT로 만들어지는 과정에
+            참여하고,
+            <br />
+            제대로 변환이 이루어지는지 감시할 수 있습니다.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <span>EL Bridge (On-boarding) -&gt;</span>
+            <br />
+            <span>Real world asset into digital NFT.</span>
+            <br />
+            실제 부동산을 NFT로 만드는 과정.
+          </div>
+          <p>
+            사용자들은 DAO를 통해 실제 부동산이 NFT로 만들어지는 과정에
+            참여하고,
+            <br />
+            제대로 변환이 이루어지는지 감시할 수 있습니다.
+          </p>
+        </div>
+        <div>이미지</div>
+      </div>
+      <div>
+        <div>이미지</div>
+        <div>
+          <div>
+            <span>&lt;- EL Bridge (On-boarding)</span>
+            <br />
+            <span>Real world asset into digital NFT.</span>
+            <br />
+            실제 부동산을 NFT로 만드는 과정.
+          </div>
+          <p>
+            사용자들은 DAO를 통해 실제 부동산이 NFT로 만들어지는 과정에
+            참여하고,
+            <br />
+            제대로 변환이 이루어지는지 감시할 수 있습니다.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <span>EL Bridge (On-boarding) -&gt;</span>
+            <br />
+            <span>Real world asset into digital NFT.</span>
+            <br />
+            실제 부동산을 NFT로 만드는 과정.
+          </div>
+          <p>
+            사용자들은 DAO를 통해 실제 부동산이 NFT로 만들어지는 과정에
+            참여하고,
+            <br />
+            제대로 변환이 이루어지는지 감시할 수 있습니다.
+          </p>
+        </div>
+        <div>이미지</div>
+      </div>
+      <div>
+        <div>이미지</div>
+        <div>
+          <div>
+            <span> &lt;- EL Bridge (On-boarding)</span>
+            <br />
+            <span>Real world asset into digital NFT.</span>
+            <br />
+            실제 부동산을 NFT로 만드는 과정.
+          </div>
+          <p>
+            사용자들은 DAO를 통해 실제 부동산이 NFT로 만들어지는 과정에
+            참여하고,
+            <br />
+            제대로 변환이 이루어지는지 감시할 수 있습니다.
+          </p>
+        </div>
+      </div>
+    </MainSectionWrapper>
+  );
+};
+
+export default Section;
