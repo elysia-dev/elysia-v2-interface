47ad40b51145ef6986ef56e4e8e970aeec2ca8ad
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-22 14:28:16 +0900

    Add Developers page

---
+++ components/Developers/Section.tsx
@@ -0,0 +1,71 @@
+import { SectionWrapper } from './styles';
+
+const Section = () => {
+  return (
+    <SectionWrapper>
+      <div>
+        <div>
+          <span>Developers</span>
+          <br />
+          <span>Give powers for contributing developers.</span>
+          <br />
+          사용자들은 자유롭게 ELYSIA 시스템을 구성하는 코드를 확인하고,
+          <br /> 자신만의 방법으로 contribute하고 보상을 획득할 수 있습니다.
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <div>What is ELYSIA?</div>
+            <div>화살표</div>
+          </div>
+          <div>
+            <div>
+              DAO for the digital transformation
+              <br /> of real estate assets.
+            </div>
+            <div>아이콘</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>What is ELYSIA?</div>
+            <div>화살표</div>
+          </div>
+          <div>
+            <div>
+              DAO for the digital transformation
+              <br /> of real estate assets.
+            </div>
+            <div>아이콘</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>What is ELYSIA?</div>
+            <div>화살표</div>
+          </div>
+          <div>
+            <div>
+              DAO for the digital transformation
+              <br /> of real estate assets.
+            </div>
+            <div>아이콘</div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>ELYSIA v1 Documentation</div>
+            <div>아이콘</div>
+          </div>
+          <div>
+            <div>Github</div>
+            <div>아이콘</div>
+          </div>
+        </div>
+      </div>
+    </SectionWrapper>
+  );
+};
+
+export default Section;
---
+++ components/Developers/index.tsx
@@ -0,0 +1,12 @@
+import Section from './Section';
+import { DevelopersWrapper } from './styles';
+
+const Developers = () => {
+  return (
+    <DevelopersWrapper>
+      <Section />
+    </DevelopersWrapper>
+  );
+};
+
+export default Developers;
