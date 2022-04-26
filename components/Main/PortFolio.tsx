46553d98b9c40659656dceadd1d065e0363ae261
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-26 11:16:46 +0900

    Modify portfolio image

--- components/Main/PortFolio.tsx
+++ components/Main/PortFolio.tsx
@@ -1,57 +1,52 @@
-import axios from 'axios';
 import Slate, { baseUrl } from 'clients/Slate';
-import { IAssetBond } from 'core/types/reserveSubgraph';
 import useReserveData from 'hooks/useReserveData';
-import Image from 'next/image';
 import Link from 'next/link';
-import { off } from 'process';
 import { useCallback, useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import AssetItem from './AssetItem';
 import { MainPortFolioWrapper } from './styles';
 
 const PortFolio = () => {
-  const { getAssetBondsByNetwork } = useReserveData();
+  const { reserveState, getAssetBondsByNetwork } = useReserveData();
   const { t, i18n } = useTranslation();
   const [image, setImage] = useState([]);
 
   const assetBonds = useMemo(() => {
     return getAssetBondsByNetwork();
-  }, [getAssetBondsByNetwork]);
+  }, [reserveState]);
 
-  // const fetchImage = useCallback(async () => {
-  //   const images: any = [];
-  //   console.log('assetBonds', assetBonds);
-  //   Promise.all(
-  //     Array(3)
-  //       .fill(0)
-  //       .map(async (_, idx) => {
-  //         if (
-  //           assetBonds[idx].id ===
-  //           '115792089237316195422007842550160057480242544124026915590235438085798243682305'
-  //         ) {
-  //           return images.push(
-  //             'https://elysia-public.s3.ap-northeast-2.amazonaws.com/elyfi/borrow01.png',
-  //           );
-  //         }
-  //         try {
-  //           const response = await Slate.fetctABTokenIpfs(
-  //             assetBonds[idx].ipfsHash || '',
-  //           );
-  //           images.push(`${baseUrl}/${response.data.images[0]?.hash}`);
-  //         } catch (error) {
-  //           console.error(error);
-  //         }
-  //       }),
-  //   ).then((res) => {
-  //     console.log('res', images);
-  //   });
-  // }, [assetBonds]);
+  const fetchImage = useCallback(async () => {
+    const images: any = [];
+    Promise.all(
+      Array(3)
+        .fill(0)
+        .map(async (_, idx) => {
+          if (
+            assetBonds[idx].id ===
+            '115792089237316195422007842550160057480242544124026915590235438085798243682305'
+          ) {
+            return images.push(
+              'https://elysia-public.s3.ap-northeast-2.amazonaws.com/elyfi/borrow01.png',
+            );
+          }
+          try {
+            const response = await Slate.fetctABTokenIpfs(
+              assetBonds[idx].ipfsHash || '',
+            );
+            images.push(`${baseUrl}/${response.data.images[0]?.hash}`);
+          } catch (error) {
+            console.error(error);
+          }
+        }),
+    ).then((res) => {
+      setImage(images);
+    });
+  }, [assetBonds]);
 
-  // useEffect(() => {
-  //   if (assetBonds.length === 0) return;
-  //   fetchImage();
-  // }, [assetBonds]);
+  useEffect(() => {
+    if (assetBonds.length === 0) return;
+    fetchImage();
+  }, [assetBonds]);
 
   return (
     <MainPortFolioWrapper>
@@ -61,27 +56,12 @@ const PortFolio = () => {
         {t(`main.portfolio.2`)}
       </div>
       <div>
-        {Array(3)
-          .fill(0)
-          .map((_, idx) => {
-            return <AssetItem key={`item_${idx}`} image={'image'} />;
-          })}
-        {/* <div>
-          <div>이미지</div>
-          <div>
-            <div>부동산</div>
-            <div>아이콘</div>
-            <div>부동산 value</div>
-          </div>
-        </div>
-        <div>
-          <div>이미지</div>
-          <div>
-            <div>부동산</div>
-            <div>아이콘</div>
-            <div>부동산 value</div>
-          </div>
-        </div> */}
+        {image.length > 0 &&
+          Array(3)
+            .fill(0)
+            .map((_, idx) => {
+              return <AssetItem key={`item_${idx}`} image={image[idx]} />;
+            })}
       </div>
       <div>
         <Link href={`${i18n.language}/Ecosystem`} passHref>
