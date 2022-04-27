ce19011489e6950d8a1f0c7489b01134513107f2
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-28 03:24:59 +0900

    Modify image size

--- components/Main/AssetItem.tsx
+++ components/Main/AssetItem.tsx
@@ -10,17 +10,11 @@ const AssetItem: React.FC<Props> = ({ image }) => {
       <div>
         <Image
           src={image}
-          //   src={image}
           alt={'assetBonds'}
-          width={400}
-          height={400}
+          width={410}
+          height={410}
           unoptimized={true}
         />
-      </div>
-      <div>
-        <div>부동산</div>
-        <div>아이콘</div>
-        <div>부동산 value</div>
       </div>
     </div>
   );
