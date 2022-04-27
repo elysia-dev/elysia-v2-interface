9d4d4c8a6665e0d93235533c5ce4c047e2a42d26
Jung gyu cheol <kcpooh0202@naver.com>
2022-04-27 11:14:07 +0900

    Modify data type array to object

--- clients/StakingFetcher.ts
+++ clients/StakingFetcher.ts
@@ -2,25 +2,25 @@ import { ERC20, StakingPoolV2 } from '@elysia-dev/elyfi-v1-sdk';
 
 export const StakingInfoFetcher =
   (): any =>
-  (...args: [StakingPoolV2, string]) => {
+  async (...args: [StakingPoolV2, string]) => {
     const [...params] = args;
     if (!params[0] || !params[1]) return undefined;
 
-    return Promise.all([
-      params[0].getPoolData(),
-      params[0].getUserReward(params[1]),
-      params[0].getUserData(params[1]),
-    ]);
+    return {
+      poolData: await params[0].getPoolData(),
+      userReward: await params[0].getUserReward(params[1]),
+      userData: await params[0].getUserData(params[1]),
+    };
   };
 
 export const tokenInfoFetcher =
   (): any =>
-  (...args: [ERC20, string, string]) => {
+  async (...args: [ERC20, string, string]) => {
     const [...params] = args;
     if (!params[0] || !params[1]) return undefined;
 
-    return Promise.all([
-      params[0].balanceOf(params[1]),
-      params[0].allowance(params[1], params[2]),
-    ]);
+    return {
+      balance: await params[0].balanceOf(params[1]),
+      allowance: await params[0].allowance(params[1], params[2]),
+    };
   };
