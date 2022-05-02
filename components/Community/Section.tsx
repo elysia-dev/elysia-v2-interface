b1d1bcac5cc1c78323ea88b49e6fcd7c68fcb557
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-03 05:36:16 +0900

    Modify community

--- components/Community/Section.tsx
+++ components/Community/Section.tsx
@@ -1,114 +1,211 @@
+import { NoiseSvg } from 'components/Main/styles';
 import { Trans, useTranslation } from 'react-i18next';
-import { CommunityWrapper, SectionWrapper } from './styles';
+import { SectionWrapper } from './styles';
+import Image from 'next/image';
+import Arrow from 'assets/images/community/arrow@2x.png';
+import Discord from 'assets/images/community/discord_white@2x.png';
+import Git from 'assets/images/community/git@2x.png';
+import Facebook from 'assets/images/community/facebook@2x.png';
+import Kakao from 'assets/images/community/kakao_white@2x.png';
+import Medium from 'assets/images/community/medium@2x.png';
+import Elysia from 'assets/images/community/elysia_icon@2x.png';
+import Snapshot from 'assets/images/community/snapshot@2x.png';
+import Tally from 'assets/images/community/tally@2x.png';
+import Telegram from 'assets/images/community/telegram_white@2x.png';
+import Twitter from 'assets/images/community/twitter@2x.png';
+import We_chat from 'assets/images/community/wechat_@2x.png';
+import Wechat from 'assets/images/community/wechat@2x.png';
+import useIsMobile from 'hooks/useIsMobile';
 
 const Section = () => {
   const { t } = useTranslation();
+  const { isDesktop } = useIsMobile();
 
   return (
-    <SectionWrapper>
-      <div>
+    <>
+      <NoiseSvg>
+        <filter id="noise">
+          <feTurbulence type="fractalNoise" baseFrequency="5.6" />
+        </filter>
+      </NoiseSvg>
+      <SectionWrapper>
         <div>
-          <span>{t('community.0')}</span>
-          <br />
-          <span>{t('community.1')}</span>
-          <br />
-          <Trans>{t('community.2')}</Trans>
-        </div>
-      </div>
-      <div>
-        <div>
-          <div>{t('community.3')}</div>
           <div>
+            <div>{t('community.0')}</div>
+            <div>{t('community.1')}</div>
+            <Trans>{t('community.2')}</Trans>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>{t('community.3')}</div>
             <div>
-              <div>아이콘</div>
-              <div>Discode</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>Telegram</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>Kakaotalk</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>WeChat</div>
-              <div>화살표</div>
+              <div>
+                <div>
+                  <Image src={Discord} alt={'Discord'} width={30} height={30} />
+                </div>
+                <div>Discode</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image
+                    src={Telegram}
+                    alt={'Telegram'}
+                    width={31}
+                    height={31}
+                  />
+                </div>
+                <div>Telegram</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image src={Kakao} alt={'Kakao'} width={29} height={29} />
+                </div>
+                <div>Kakaotalk</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image src={We_chat} alt={'Wechat'} width={30} height={30} />
+                </div>
+                <div>WeChat</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
         <div>
-          <div>{t('community.4')}</div>
           <div>
+            <div>{t('community.4')}</div>
             <div>
-              <div>아이콘</div>
-              <div>Discode</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>Telegram</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>Kakaotalk</div>
-              <div>화살표</div>
+              <div>
+                <div>
+                  <Image
+                    src={Elysia}
+                    alt={'Elysia'}
+                    width={27}
+                    height={29.72}
+                  />
+                </div>
+                <div>Forum</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image src={Tally} alt={'Tally'} width={20} height={28} />
+                </div>
+                <div>Tally</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image
+                    src={Snapshot}
+                    alt={'Snapshot'}
+                    width={23}
+                    height={27}
+                  />
+                </div>
+                <div>Snapshot</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div></div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
         <div>
-          <div>{t('community.5')}</div>
           <div>
+            <div>{t('community.5')}</div>
             <div>
-              <div>아이콘</div>
-              <div>Discode</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>Telegram</div>
-              <div>화살표</div>
+              <div>
+                <div>
+                  <Image src={Git} alt={'Git'} width={34.42} height={34.42} />
+                </div>
+                <div>Github</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image src={Medium} alt={'Medium'} width={38} height={21} />
+                </div>
+                <div>Medium</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div></div>
+              <div></div>
             </div>
           </div>
         </div>
-      </div>
-      <div>
         <div>
-          <div>{t('community.6')}</div>
           <div>
+            <div>{t('community.6')}</div>
             <div>
-              <div>아이콘</div>
-              <div>Discode</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>Telegram</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>Kakaotalk</div>
-              <div>화살표</div>
-            </div>
-            <div>
-              <div>아이콘</div>
-              <div>WeChat</div>
-              <div>화살표</div>
+              <div>
+                <div>
+                  <Image src={Twitter} alt={'Twitter'} width={31} height={31} />
+                </div>
+                <div>Twitter</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image
+                    src={Facebook}
+                    alt={'Facebook'}
+                    width={33.52}
+                    height={33.52}
+                  />
+                </div>
+                <div>Facebook</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image src={Medium} alt={'Medium'} width={38} height={21} />
+                </div>
+                <div>Medium</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
+              <div>
+                <div>
+                  <Image src={Wechat} alt={'Wechat'} width={32} height={26} />
+                </div>
+                <div>WeChat</div>
+                <div>
+                  <Image src={Arrow} alt={'Arrow'} width={26} height={26} />
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </SectionWrapper>
+      </SectionWrapper>
+    </>
   );
 };
 
--- components/Community/styles.ts
+++ components/Community/styles.ts
@@ -9,134 +9,204 @@ export const center = {
 };
 
 export const CommunityWrapper = styled.div`
-  /* padding: 0px 45px 50px 45px; */
-  background: #dfd9d9;
+  position: relative;
+  overflow: 'hidden';
 `;
 
 export const SectionWrapper = styled.div`
   width: 100%;
-  /* height: 95vh; */
+  padding: 0px 141px 100px 141px;
+  color: #ffffff;
+  font-family: Gilroy-Light;
+  &::before,
+  &::after {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    mix-blend-mode: color-burn;
+    content: '';
+  }
+
+  &::before {
+    filter: url(#noise);
+  }
+
+  &::after {
+    background: linear-gradient(to right, #000000, transparent),
+      linear-gradient(to top, #000000, #4785ff 40%);
+  }
 
   > div:nth-child(1) {
-    /* padding-top: 400px; */
-    background-color: #fff2ff;
-    padding: 400px 45px 50px 45px;
+    padding-top: 634px;
+
+    margin-bottom: 30px;
+    /* padding: 400px 45px 50px 45px; */
     > div:first-child {
-      border: ${defaultBorder};
       width: 50%;
+      color: #cbcbcb;
+      font-size: 1.375rem;
+      > div:first-child {
+        font-family: Gilroy-ExtraBold;
+        font-size: 2rem;
+      }
+      > div:nth-child(2) {
+        color: #33a5ff;
+      }
     }
   }
+  > div:nth-child(2),
+  > div:nth-child(3),
+  > div:nth-child(4) {
+    margin-bottom: 25px;
+  }
   > div:nth-child(2) {
-    border: ${defaultBorder};
-    padding: 45px;
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 0px 6px #00000029;
+    border-radius: 20px;
+    backdrop-filter: blur(18px);
+    padding: 45px 41px;
     > div:first-child {
-      border: ${defaultBorder};
       padding: 10px;
       > div:last-child {
         margin-top: 13px;
         display: flex;
-        /* justify-content: space-between; */
+        justify-content: space-between;
         > div {
-          width: 250px;
-          border: ${defaultBorder};
+          font-family: Gilroy-ExtraBold;
+          font-size: 1.375rem;
+          width: 310px;
+          height: 46px;
+          box-shadow: 0px 0px 6px #00000029;
+          background-color: #000000;
+          border-radius: 23px;
           ${center}
-          padding: 5px;
+          padding: 0px 25px 0px 29.5px;
           > div:nth-child(1) {
-            margin-right: 10px;
+            margin-right: 15px;
+            ${center}
           }
           > div:nth-child(3) {
             margin-left: auto;
           }
         }
-        > div:not(div:nth-child(4)) {
-          margin-right: auto;
-        }
+        /* > div:not(div:nth-child(4)) {
+          margin-right: 104px;
+        } */
       }
     }
   }
   > div:nth-child(3) {
-    border: ${defaultBorder};
-    padding: 45px;
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 0px 6px #00000029;
+    border-radius: 20px;
+    backdrop-filter: blur(18px);
+    padding: 45px 41px;
     > div:first-child {
-      border: ${defaultBorder};
-      display: inline-block;
       padding: 10px;
       > div:last-child {
         margin-top: 13px;
         display: flex;
-        justify-content: flex-start;
-        > div {
-          width: 250px;
-          border: ${defaultBorder};
+        justify-content: space-between;
+        > div:not(div:last-child) {
+          font-family: Gilroy-ExtraBold;
+          font-size: 1.375rem;
+          width: 310px;
+          height: 46px;
+          box-shadow: 0px 0px 6px #00000029;
+          background-color: #000000;
+          border-radius: 23px;
           ${center}
-          padding: 5px;
+          padding: 0px 25px 0px 29.5px;
           > div:nth-child(1) {
-            margin-right: 10px;
+            margin-right: 15px;
+            ${center}
           }
           > div:nth-child(3) {
             margin-left: auto;
           }
         }
-        > div:not(div:nth-child(3)) {
-          margin-right: 32.328px;
+        > div:last-child {
+          width: 310px;
         }
       }
     }
   }
   > div:nth-child(4) {
-    border: ${defaultBorder};
-    padding: 45px;
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 0px 6px #00000029;
+    border-radius: 20px;
+    backdrop-filter: blur(18px);
+    padding: 45px 41px;
     > div:first-child {
-      border: ${defaultBorder};
-      display: inline-block;
       padding: 10px;
       > div:last-child {
         margin-top: 13px;
         display: flex;
-        justify-content: flex-start;
-        > div {
-          width: 250px;
-          border: ${defaultBorder};
+        justify-content: space-between;
+        > div:not(div:nth-child(3), div:nth-child(4)) {
+          font-family: Gilroy-ExtraBold;
+          font-size: 1.375rem;
+          width: 310px;
+          height: 46px;
+          box-shadow: 0px 0px 6px #00000029;
+          background-color: #000000;
+          border-radius: 23px;
           ${center}
-          padding: 5px;
+          padding: 0px 25px 0px 29.5px;
           > div:nth-child(1) {
-            margin-right: 10px;
+            margin-right: 15px;
+            ${center}
           }
           > div:nth-child(3) {
             margin-left: auto;
           }
         }
-        > div:not(div:nth-child(2)) {
-          margin-right: 32.328px;
+        /* > div:not(div:nth-child(4)) {
+          margin-right: 104px;
+        } */
+        > div:nth-child(3),
+        > div:nth-child(4) {
+          width: 310px;
         }
       }
     }
   }
   > div:nth-child(5) {
-    border: ${defaultBorder};
-    padding: 45px;
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 0px 6px #00000029;
+    border-radius: 20px;
+    backdrop-filter: blur(18px);
+    padding: 45px 41px;
     > div:first-child {
-      border: ${defaultBorder};
       padding: 10px;
       > div:last-child {
         margin-top: 13px;
         display: flex;
-        /* justify-content: space-between; */
+        justify-content: space-between;
         > div {
-          width: 250px;
-          border: ${defaultBorder};
+          font-family: Gilroy-ExtraBold;
+          font-size: 1.375rem;
+          width: 310px;
+          height: 46px;
+          box-shadow: 0px 0px 6px #00000029;
+          background-color: #000000;
+          border-radius: 23px;
           ${center}
-          padding: 5px;
+          padding: 0px 25px 0px 29.5px;
           > div:nth-child(1) {
-            margin-right: 10px;
+            margin-right: 15px;
+            ${center}
           }
           > div:nth-child(3) {
             margin-left: auto;
           }
         }
-        > div:not(div:nth-child(4)) {
-          margin-right: auto;
-        }
+        /* > div:not(div:nth-child(4)) {
+          margin-right: 104px;
+        } */
       }
     }
   }
