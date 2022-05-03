ceabc66d3f06ce49b565230fed4fb195c7c04897
Jung Gyu Cheol <kcpooh0202@naver.com>
2022-05-03 16:14:07 +0900

    Add isTablet

--- hooks/useIsMobile.ts
+++ hooks/useIsMobile.ts
@@ -4,19 +4,24 @@ import { useMediaQuery } from 'react-responsive';
 const useIsMobile = () => {
   const [isMobile, setIsMobile] = useState(false);
   const [isDesktop, setIsDesktop] = useState(false);
+  const [isTablet, setIsTablet] = useState(false);
   const mobile = useMediaQuery({
     query: '(min-width:0px) and (max-width:768px)',
   });
   const desktop = useMediaQuery({
     query: '(min-width:769px) and (max-width:1299px)',
   });
+  const tablet = useMediaQuery({
+    query: '(min-width:0px) and (max-width:959px)',
+  });
 
   useEffect(() => {
     setIsMobile(mobile);
     setIsDesktop(desktop);
-  }, [mobile, desktop]);
+    setIsTablet(tablet);
+  }, [mobile, desktop, tablet]);
 
-  return { isMobile, isDesktop };
+  return { isMobile, isDesktop, isTablet };
 };
 
 export default useIsMobile;
