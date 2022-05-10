4bfa08d583568e7090d605107f9eac2cf79ff10f
Jung Gyu Cheol <kcpooh0202@naver.com>
2022-05-10 11:51:47 +0900

    Add svg

--- components/Documents/Section.tsx
+++ components/Documents/Section.tsx
@@ -1,71 +1,92 @@
+import { NoiseSvg } from 'components/Main/styles';
 import { Trans, useTranslation } from 'react-i18next';
 import { SectionWrapper } from './styles';
+import Elysia from 'assets/images/developers/elysia.svg';
+import Git from 'assets/images/developers/git.svg';
+import Arrow from 'assets/images/developers/arrow.svg';
+import Elyfi from 'assets/images/developers/elyfi.svg';
+import Guide from 'assets/images/developers/guide.svg';
+import useIsMobile from 'hooks/useIsMobile';
 
 const Section = () => {
   const { t } = useTranslation();
+  const { isTablet } = useIsMobile();
 
   return (
-    <SectionWrapper>
-      <div>
-        <div>
-          <span>{t('documents.0')}</span>
-          <br />
-          <span>{t('documents.1')}</span>
-          <br />
-          <Trans>{t('documents.2')}</Trans>
-        </div>
-      </div>
-      <div>
+    <>
+      <NoiseSvg>
+        <filter id="noise">
+          <feTurbulence type="fractalNoise" baseFrequency="5.6" />
+        </filter>
+      </NoiseSvg>
+      <SectionWrapper>
         <div>
           <div>
-            <div>{t('documents.3')}</div>
-            <div>화살표</div>
+            <div>{t('documents.0')}</div>
+            <div>{t('documents.1')}</div>
+            <Trans>{t('documents.2')}</Trans>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <div>
+                <Elysia />
+              </div>
+              <div>{t('documents.3')}</div>
+              <div>{!isTablet && <Arrow />}</div>
+            </div>
+            <div>
+              <div>
+                <Trans> {t('documents.4')}</Trans>
+              </div>
+            </div>
           </div>
           <div>
             <div>
-              <Trans> {t('documents.4')}</Trans>
+              <div>
+                <Elyfi />
+              </div>
+              <div>{t('documents.5')}</div>
+              <div>{!isTablet && <Arrow />}</div>
             </div>
-            <div>아이콘</div>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div>{t('documents.5')}</div>
-            <div>화살표</div>
+            <div>
+              <div>
+                <Trans>{t('documents.6')}</Trans>
+              </div>
+            </div>
           </div>
           <div>
             <div>
-              <Trans>{t('documents.6')}</Trans>
+              <div>
+                <Guide />
+              </div>
+              <div>{t('documents.7')}</div>
+              <div>{!isTablet && <Arrow />}</div>
             </div>
-            <div>아이콘</div>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div>{t('documents.7')}</div>
-            <div>화살표</div>
+            <div>
+              <div>
+                <Trans>{t('documents.8')}</Trans>
+              </div>
+            </div>
           </div>
           <div>
             <div>
-              <Trans>{t('documents.8')}</Trans>
+              <div>
+                <Git />
+              </div>
+              <div>{t('documents.9')}</div>
+              <div>{!isTablet && <Arrow />}</div>
             </div>
-            <div>아이콘</div>
-          </div>
-        </div>
-        <div>
-          <div>
-            <div>{t('documents.9')}</div>
-            <div>화살표</div>
-          </div>
-          <div>
             <div>
-              <Trans>{t('documents.10')}</Trans>
+              <div>
+                <Trans>{t('documents.10')}</Trans>
+              </div>
             </div>
-            <div>아이콘</div>
           </div>
         </div>
-      </div>
-    </SectionWrapper>
+      </SectionWrapper>
+    </>
   );
 };
 
