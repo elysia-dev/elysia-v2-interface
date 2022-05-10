ade15b164adca140136bf675515e4ba10793d267
Jung Gyu Cheol <kcpooh0202@naver.com>
2022-05-10 11:51:15 +0900

    Modify document style

--- components/Documents/styles.ts
+++ components/Documents/styles.ts
@@ -2,39 +2,92 @@ import styled from 'styled-components';
 
 const defaultBorder = '1px solid #333333';
 
+const glassBox = {
+  background: 'rgba(255, 255, 255, 0.1)',
+  'box-shadow': '0px 0px 6px #00000029',
+  'border-radius': '20px',
+  'backdrop-filter': 'blur(18px)',
+};
+
 export const DocumentsWrapper = styled.div`
   /* padding: 0px 45px 50px 45px; */
-  background: #dfd9d9;
+  /* background: #dfd9d9; */
 `;
 
 export const SectionWrapper = styled.div`
-  width: 100%;
-  /* height: 95vh; */
+  height: 100vh;
+  max-width: 1639px;
+  margin: auto;
+  padding: 0px 20px 0px 20px;
+  color: #ffffff;
+  font-family: Gilroy-Light;
+
+  &::before,
+  &::after {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    mix-blend-mode: hard-light;
+    content: '';
+  }
+
+  &::before {
+    filter: url(#noise);
+  }
+
+  &::after {
+    background: linear-gradient(to right, #000000, transparent),
+      linear-gradient(to top, #000000, #4785ff 40%);
+  }
 
   > div:nth-child(1) {
-    /* padding-top: 400px; */
-    background-color: #fff2ff;
-    padding: 400px 45px 50px 45px;
+    padding-top: 634px;
+    margin-bottom: 30px;
     > div:first-child {
-      border: ${defaultBorder};
-      width: 50%;
+      color: #cbcbcb;
+      font-size: 1.375rem;
+      > div:first-child {
+        color: #ffffff;
+        font-family: Gilroy-ExtraBold;
+        font-size: 2rem;
+        margin-bottom: 5px;
+      }
+      > div:nth-child(2) {
+        color: #33a5ff;
+        margin-bottom: 14px;
+      }
     }
   }
   > div:nth-child(2) {
-    border: ${defaultBorder};
-    padding: 0px 45px;
     display: flex;
     justify-content: space-between;
     > div {
-      width: 255px;
-      border: ${defaultBorder};
+      width: 24.5%;
+      ${glassBox}
+      padding: 38.22px 25px 38px 25px;
       > div:first-child {
         display: flex;
+        align-items: center;
+        font-family: Gilroy-ExtraBold;
+        font-size: 1.25rem;
+        > div {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
         > div:first-child {
+          margin-right: 10px;
+        }
+        > div:nth-child(2) {
           margin-right: auto;
         }
       }
       > div:last-child {
+        font-size: 1rem;
+        color: #cbcbcb;
         margin-top: 10px;
         display: flex;
         > div:first-child {
