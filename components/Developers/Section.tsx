0aa15219bdee4f554594d825000b35ad75d72287
Jung Gyu Cheol <kcpooh0202@naver.com>
2022-05-10 11:51:33 +0900

    Modify image to svg

--- components/Developers/Section.tsx
+++ components/Developers/Section.tsx
@@ -1,18 +1,17 @@
 import { NoiseSvg } from 'components/Main/styles';
 import useIsMobile from 'hooks/useIsMobile';
-import Image from 'next/image';
 import { Trans, useTranslation } from 'react-i18next';
 import { SectionWrapper } from './styles';
-import Arrow from 'assets/images/community/arrow@2x.png';
 import Smart from 'assets/images/developers/smart.svg';
 import Bug from 'assets/images/developers/bug.svg';
 import PaperFile from 'assets/images/developers/paperFile.svg';
 import Elysia from 'assets/images/developers/elysia.svg';
 import Git from 'assets/images/developers/git.svg';
+import Arrow from 'assets/images/developers/arrow.svg';
 
 const Section = () => {
   const { t } = useTranslation();
-  const { isDesktop, isTablet } = useIsMobile();
+  const { isTablet } = useIsMobile();
   return (
     <>
       <NoiseSvg>
@@ -35,17 +34,7 @@ const Section = () => {
                 <Elysia />
               </div>
               <div>{t('developers.3')}</div>
-              <div>
-                {' '}
-                {!isTablet && (
-                  <Image
-                    src={Arrow}
-                    alt={'Arrow'}
-                    width={isDesktop ? 20 : 26}
-                    height={isDesktop ? 20 : 26}
-                  />
-                )}
-              </div>
+              <div> {!isTablet && <Arrow />}</div>
             </div>
             <div>
               <div>
@@ -59,16 +48,7 @@ const Section = () => {
                 <Smart />
               </div>
               <div>{t('developers.5')}</div>
-              <div>
-                {!isTablet && (
-                  <Image
-                    src={Arrow}
-                    alt={'Arrow'}
-                    width={isDesktop ? 20 : 26}
-                    height={isDesktop ? 20 : 26}
-                  />
-                )}
-              </div>
+              <div>{!isTablet && <Arrow />}</div>
             </div>
             <div>
               <div>
@@ -82,16 +62,7 @@ const Section = () => {
                 <Bug />
               </div>
               <div>{t('developers.7')}</div>
-              <div>
-                {!isTablet && (
-                  <Image
-                    src={Arrow}
-                    alt={'Arrow'}
-                    width={isDesktop ? 20 : 26}
-                    height={isDesktop ? 20 : 26}
-                  />
-                )}
-              </div>
+              <div>{!isTablet && <Arrow />}</div>
             </div>
             <div>
               <div>
@@ -105,32 +76,14 @@ const Section = () => {
                 <PaperFile />
               </div>
               <div>{t('developers.9')}</div>
-              <div>
-                {!isTablet && (
-                  <Image
-                    src={Arrow}
-                    alt={'Arrow'}
-                    width={isDesktop ? 20 : 26}
-                    height={isDesktop ? 20 : 26}
-                  />
-                )}
-              </div>
+              <div>{!isTablet && <Arrow />}</div>
             </div>
             <div>
               <div>
                 <Git />
               </div>
               <div>{t('developers.10')}</div>
-              <div>
-                {!isTablet && (
-                  <Image
-                    src={Arrow}
-                    alt={'Arrow'}
-                    width={isDesktop ? 20 : 26}
-                    height={isDesktop ? 20 : 26}
-                  />
-                )}
-              </div>
+              <div>{!isTablet && <Arrow />}</div>
             </div>
           </div>
         </div>
