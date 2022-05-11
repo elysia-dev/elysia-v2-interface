cde9b6b7bc5ac03dce556a35db426b6a3a246394
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-11 18:35:56 +0900

    Remove styled

--- components/Ecosystem/Top.tsx
+++ components/Ecosystem/Top.tsx
@@ -1,6 +1,6 @@
 import { NoiseSvg } from 'components/Main/styles';
 import { Trans, useTranslation } from 'react-i18next';
-import { ContentWrapper, ProjectWrapper, TopWrapper } from './styles';
+import { ProjectWrapper, TopWrapper } from './styles';
 import Arrow from 'assets/images/developers/arrow.svg';
 import useIsMobile from 'hooks/useIsMobile';
 import Image from 'next/image';
@@ -18,69 +18,62 @@ const Top = () => {
           <feTurbulence type="fractalNoise" baseFrequency="5.6" />
         </filter>
       </NoiseSvg>
-      <ContentWrapper>
-        <TopWrapper>
+      <TopWrapper>
+        <div>
+          <div>{t('ecosystem.top.0')}</div>
           <div>
-            <div>{t('ecosystem.top.0')}</div>
+            <Trans>{t('ecosystem.top.1')}</Trans>
+          </div>
+        </div>
+      </TopWrapper>
+      <ProjectWrapper>
+        <div>
+          <div>{t('ecosystem.project.0')}</div>
+          <div>
+            <Trans>{t('ecosystem.project.1')}</Trans>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div />
             <div>
-              <Trans>{t('ecosystem.top.1')}</Trans>
+              <div>ELYFI</div>
+              <div>{!isTablet && <Arrow />}</div>
+            </div>
+            <div>
+              <Trans>{t('ecosystem.project.2')}</Trans>
             </div>
           </div>
-        </TopWrapper>
-        <ProjectWrapper>
           <div>
-            <div>{t('ecosystem.project.0')}</div>
             <div>
-              <Trans>{t('ecosystem.project.1')}</Trans>
+              <div>{t('ecosystem.project.3')}</div>
+            </div>
+            <div>
+              <div>{t('ecosystem.project.4')}</div>
+              <div>
+                <Image src={Google} alt={'google'} width={38} height={38} />
+              </div>
+              <div>
+                <Image src={Appstore} alt={'Appstore'} width={38} height={38} />
+              </div>
+            </div>
+            <div>
+              <Trans>{t('ecosystem.project.5')}</Trans>
             </div>
           </div>
           <div>
             <div>
-              <div />
-              <div>
-                <div>ELYFI</div>
-                <div>{!isTablet && <Arrow />}</div>
-              </div>
-              <div>
-                <Trans>{t('ecosystem.project.2')}</Trans>
-              </div>
+              <div>Coming Soon!</div>
             </div>
             <div>
-              <div>
-                <div>{t('ecosystem.project.3')}</div>
-              </div>
-              <div>
-                <div>{t('ecosystem.project.4')}</div>
-                <div>
-                  <Image src={Google} alt={'google'} width={38} height={38} />
-                </div>
-                <div>
-                  <Image
-                    src={Appstore}
-                    alt={'Appstore'}
-                    width={38}
-                    height={38}
-                  />
-                </div>
-              </div>
-              <div>
-                <Trans>{t('ecosystem.project.5')}</Trans>
-              </div>
+              <div>{t('ecosystem.project.6')}</div>
             </div>
             <div>
-              <div>
-                <div>Coming Soon!</div>
-              </div>
-              <div>
-                <div>{t('ecosystem.project.6')}</div>
-              </div>
-              <div>
-                <Trans>{t('ecosystem.project.7')}</Trans>
-              </div>
+              <Trans>{t('ecosystem.project.7')}</Trans>
             </div>
           </div>
-        </ProjectWrapper>
-      </ContentWrapper>
+        </div>
+      </ProjectWrapper>
     </>
   );
 };
