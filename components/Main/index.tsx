bd850aa157cd170c60cd6ece548c2ba123a3354e
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-11 18:18:55 +0900

    Modify browser height

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,18 +1,28 @@
 import MainTopImg from 'assets/images/mainTop@2x.png';
-import { MainWrapper, TopPortfolioWrapper } from './styles';
+import { MainWrapper } from './styles';
 import PortFolio from './PortFolio';
 import Top from './Top';
 import Section from './Section';
 import Partners from './Partners';
 import Team from './Team';
+import { useEffect, useState } from 'react';
 
 const Main = () => {
+  const [browserHeight, setBrowserHeight] = useState(0);
+
+  useEffect(() => {
+    if (typeof document === undefined || typeof window === undefined) return;
+    if (window.innerHeight > document.body.clientHeight) {
+      const sub = window.innerHeight - document.body.clientHeight;
+      setBrowserHeight(document.body.clientHeight + sub);
+      return;
+    }
+    setBrowserHeight(document.body.clientHeight);
+  }, []);
   return (
-    <MainWrapper>
-      <TopPortfolioWrapper>
-        <Top />
-        <PortFolio />
-      </TopPortfolioWrapper>
+    <MainWrapper theme={browserHeight}>
+      <Top />
+      <PortFolio />
       <Section />
       <Partners />
       <Team />
