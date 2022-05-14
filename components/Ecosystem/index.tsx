287bab95cc35fa535dfdc14fd3fbf81584614260
Jung Gyu Cheol <kcpooh0202@naver.com>
2022-05-14 16:48:00 +0900

    Remove canvas

--- components/Ecosystem/index.tsx
+++ components/Ecosystem/index.tsx
@@ -1,37 +1,14 @@
 import CollateralCategory from 'enums/CollateralCategory';
 import useReserveData from 'hooks/useReserveData';
-import { useRouter } from 'next/router';
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import Gradient from 'utils/gradient';
+import { useMemo, useState } from 'react';
 import { parseTokenId } from 'utils/parseTokenId';
 import PortFolio from './PortFolio';
 import Top from './Top';
-import { EcosystemWrapper } from './styles';
-import useResizeBrowser from 'hooks/useResizeBrowser';
+import { EcosystemImage } from './styles';
 
 const Ecosystem = () => {
-  const { browserHeight, setResize } = useResizeBrowser();
   const { reserveState, getAssetBondsByNetwork } = useReserveData();
   const [pageNum, setPageNum] = useState(1);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const { pathname } = useRouter();
-
-  const drawCanvas = useCallback(() => {
-    const dpr = window.devicePixelRatio;
-    const canvas: HTMLCanvasElement | null = canvasRef.current;
-
-    if (!canvas) return;
-    let sub = 0;
-    if (window.innerHeight > document.body.clientHeight) {
-      sub = window.innerHeight - document.body.clientHeight;
-    }
-    canvas.width = document.body.clientWidth * dpr;
-    canvas.height = (document.body.clientHeight + sub) * dpr;
-    const ctx = canvas.getContext('2d');
-
-    if (!ctx) return;
-    new Gradient(ctx, canvas.width, canvas.height, pathname);
-  }, [pathname]);
 
   const assetBonds = useMemo(() => {
     return getAssetBondsByNetwork();
@@ -48,29 +25,16 @@ const Ecosystem = () => {
       });
   }, [assetBonds]);
 
-  useEffect(() => {
-    setResize();
-    drawCanvas();
-  }, [pageNum, assetBondTokensBackedByEstate, drawCanvas]);
-
   return (
-    <EcosystemWrapper theme={browserHeight}>
-      <canvas
-        ref={canvasRef}
-        style={{
-          position: 'absolute',
-          width: '100%',
-          top: 0,
-          left: 0,
-          zIndex: -2,
-        }}></canvas>
+    <>
+      <EcosystemImage />
       <Top />
       <PortFolio
         assetBondTokens={assetBondTokensBackedByEstate}
         pageNum={pageNum}
         setPageNum={setPageNum}
       />
-    </EcosystemWrapper>
+    </>
   );
 };
 
