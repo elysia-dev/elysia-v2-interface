535a3620b0139669eabf28ba78b386677e2921dd
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-17 18:03:36 +0900

    Add RealEstateType

---
+++ enums/ RealEstateTypes.ts
@@ -0,0 +1,8 @@
+enum RealEstateTypes {
+  RealEstateToken = 'RealEstateToken',
+  ABTokenAType = 'ABTokenAType',
+  ABTokenBType = 'ABTokenBType',
+  PFToken = 'PFToken',
+}
+
+export default RealEstateTypes;
