3bb42c24decc894796f9ecc339c3d33a8ee5a097
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-18 16:08:10 +0900

    Modify style

--- components/ELBridge/index.tsx
+++ components/ELBridge/index.tsx
@@ -1,30 +1,9 @@
-import { useEffect, useState } from 'react';
 import Section from './Section';
-import axios from 'axios';
 
 const ELBridge = () => {
-  const [totalCreatedNFT, setTotalCreatedNFT] = useState(0);
-  useEffect(() => {
-    // countapi.visits().then((result) => {
-    //   console.log(result.value);
-    // });
-    axios
-      .get(
-        'https://api.countapi.xyz/get/test/b9424cfb-a2ff-48de-997b-05af2686290b',
-      )
-      .then((res) => {
-        console.log(res);
-        setTotalCreatedNFT(res.data.value);
-      });
-  }, []);
-
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-      }}>
-      <Section totalCreatedNFT={totalCreatedNFT} />
+    <div>
+      <Section />
     </div>
   );
 };
