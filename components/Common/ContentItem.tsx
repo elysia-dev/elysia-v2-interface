11b6edce773fc073f7cc37b7a0e4ba151831b53d
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-19 20:02:12 +0900

    Modify when there is no link

--- components/Common/ContentItem.tsx
+++ components/Common/ContentItem.tsx
@@ -5,20 +5,32 @@ import Link from 'next/link';
 const ContentItem: React.FC<{
   Icon: any;
   contentName: string;
-  link: string;
+  link?: string;
 }> = ({ Icon, contentName, link }) => {
   const { isTablet } = useIsMobile();
 
   return (
-    <Link href={link} passHref>
-      <a target="_blank" rel="noopener noreferrer">
-        <div>
-          <div>{Icon}</div>
-          <div>{contentName}</div>
-          <div> {!isTablet && <Arrow />}</div>
-        </div>
-      </a>
-    </Link>
+    <>
+      {link ? (
+        <Link href={link} passHref>
+          <a target="_blank" rel="noopener noreferrer">
+            <div>
+              <div>{Icon}</div>
+              <div>{contentName}</div>
+              <div> {!isTablet && <Arrow />}</div>
+            </div>
+          </a>
+        </Link>
+      ) : (
+        <a target="_blank" rel="noopener noreferrer">
+          <div>
+            <div>{Icon}</div>
+            <div>{contentName}</div>
+            <div> {!isTablet && <Arrow />}</div>
+          </div>
+        </a>
+      )}
+    </>
   );
 };
 
