087b20c6baef31a9e97fed90d21ac845b800e9f5
Jung gyu cheol <kcpooh0202@naver.com>
2022-05-20 10:01:34 +0900

    Modify z-index

--- components/Layout/styles.ts
+++ components/Layout/styles.ts
@@ -20,7 +20,7 @@ export const LayoutNoise = styled.div`
   position: fixed;
   width: 100%;
   height: 100%;
-  z-index: 0;
+  z-index: -1;
   background-image: url(${noise.src});
   background-size: auto;
   /* &::before,
