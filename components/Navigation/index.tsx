505f873b68af081a11a286e5a9f5bc95a2d93b45
hyunoh-modori <hyunoh.bae@modori.io>
2022-06-20 18:46:45 +0900

    delete scss

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useCallback, useContext, useEffect, useState } from 'react';
 import ConnectWalletButton from './ConnectWalletButton';
-import styles from './Navigation.module.scss';
 import ElysiaLogo from 'assets/images/Elysia_Logo_White@2x.png';
 import Image from 'next/image';
 import { isMetamask, isWalletConnector } from 'utils/connectWallet';
