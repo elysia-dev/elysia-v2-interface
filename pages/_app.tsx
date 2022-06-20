0c83a1bfa30466a06e63391ea4ff568c70af6e33
hyunoh-modori <hyunoh.bae@modori.io>
2022-06-20 18:48:09 +0900

    change language provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import '../styles/globals.css';
 import '../styles/style.scss';
 import 'react-loading-skeleton/dist/skeleton.css';
 import type { AppProps } from 'next/app';
-import LanguageProvider from 'provider/LanguageProvider';
 import { Web3ReactProvider } from '@web3-react/core';
 import getLibrary from 'utils/getLibrary';
 import TxProvider from 'provider/TxProvider';
@@ -12,11 +11,16 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import * as gtag from '../lib/gtag';
 import dynamic from 'next/dynamic';
+import { useTranslation } from 'react-i18next';
+import getLocalLanguage from 'utils/getLocalLanguage';
+import LanguageType from 'enums/LanguageType';
 
 const Layout = dynamic(() => import('components/Layout'));
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { i18n } = useTranslation();
   const router = useRouter();
+  const lng = router.asPath.substring(1, 3);
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
@@ -28,14 +32,26 @@ function MyApp({ Component, pageProps }: AppProps) {
     };
   }, [router.events]);
 
+  useEffect(() => {
+    i18n.changeLanguage(getLocalLanguage());
+  }, []);
+
+  useEffect(() => {
+    if ([LanguageType.EN, LanguageType.KO].includes(lng as LanguageType)) {
+      window.localStorage.setItem('@language', lng);
+      i18n.changeLanguage(lng);
+    }
+    if (router.pathname === '/') {
+      router.push(`/${getLocalLanguage()}`);
+    }
+  }, [lng]);
+
   return (
     <Web3ReactProvider getLibrary={getLibrary}>
       <TxProvider>
-        <LanguageProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </LanguageProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </TxProvider>
     </Web3ReactProvider>
   );
