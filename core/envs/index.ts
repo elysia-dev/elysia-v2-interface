c187009b2f4009abf6bda77b92dbe730bfcc6ec9
hyunoh-modori <hyunoh.bae@modori.io>
2022-06-21 18:31:21 +0900

    delete unused envs

--- core/envs/index.ts
+++ core/envs/index.ts
@@ -2,69 +2,29 @@ import testVars from './test.json';
 import prodVars from './prod.json';
 
 interface EnvironmentVariables {
-  moneyPool: {
-    bscMoneyPoolAddress: string;
-    moneyPoolAddress: string;
-    daiTokenizerAddress: string;
-    usdtTokenizerAddress: string;
-  };
-  incentivePool: {
-    prevDaiIncentivePool: string;
-    prevUSDTIncentivePool: string;
-    currentDaiIncentivePool: string;
-    currentUSDTIncentivePool: string;
-    busdIncentivePoolAddress: string;
-  };
   staking: {
     elStakingPoolAddress: string;
     elStakingV2PoolAddress: string;
-    elfyStakingPoolAddress: string;
-    elfyV2StakingPoolAddress: string;
-    elfyV2StakingPoolRewardAddress: string;
-    elfyBscStakingPoolAddress: string;
-  };
-  lpStaking: {
-    daiElfiPoolAddress: string;
-    ethElfiPoolAddress: string;
-    stakerAddress: string;
-    nonFungiblePositionAddress: string;
-    refundedAddress: string;
   };
   token: {
-    governanceAddress: string;
     elAddress: string;
     daiAddress: string;
     usdtAddress: string;
     usdcAddress: string;
-    wEthAddress: string;
-    bscElfiAddress: string;
     busdAddress: string;
-    testBscElfiAddress: string;
-  };
-  network: {
-    requiredNetwork: string;
-    requiredChainId: number;
-    bscMainnetChainId: number;
   };
   subgraphApiEndpoint: {
     subgraphURI: string;
-    lpTokenPoolSubgraphURL: string;
-    stakerSubgraphURL: string;
     bscSubgraphURI: string;
   };
   externalApiEndpoint: {
     etherscanURI: string;
-    bscscanURI: string;
-    cachedUniswapV3URL: string;
     coingackoURL: string;
   };
   tokenizer: {
     daiTokenizerAddress: string;
     usdtTokenizerAddress: string;
     busdTokenizerAddress: string;
-  };
-  jsonRpcUrl: {
-    bsc: string;
   };
 }
 
