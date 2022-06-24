25bee84f029f8c999df7d74653e916fd223e8399
hyunoh-modori <hyunoh.bae@modori.io>
2022-06-24 13:43:18 +0900

    delete unused enums

--- enums/RecentActivityType.ts
+++ enums/RecentActivityType.ts
@@ -1,18 +1,9 @@
 enum RecentActivityType {
   ELClaim = 'Claim',
-  ELFIClaim = 'ELFIClaim',
-  ELMigration = 'ELMigration',
-  ELFIMigration = 'ELFIMigration',
-  Deposit = 'Deposit',
-  Withdraw = 'Withdraw',
-  Claim = 'Claim',
   ELStakingWithdraw = 'ELStakingWithdraw',
-  ELFIStakingWithdraw = 'ELFIStakingWithdraw',
   ELStake = 'ELStake',
-  ELFIStake = 'ELFIStake',
   Approve = 'Approve',
   Idle = 'Idle',
-  LPMigration = 'LPMigration',
 }
 
 export default RecentActivityType;
