58110a0b6aee59e1bf67cb8c83756840d406d4d2
Jung gyu cheol <kcpooh0202@naver.com>
2022-07-04 13:57:20 +0900

    Modify web3react v6 to v8

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,15 +5,26 @@ import '../styles/style.scss';
 import 'react-loading-skeleton/dist/skeleton.css';
 import type { AppProps } from 'next/app';
 import LanguageProvider from 'provider/LanguageProvider';
-import { Web3ReactProvider } from '@web3-react/core';
-import getLibrary from 'utils/getLibrary';
+import { Web3ReactHooks, Web3ReactProvider } from '@web3-react/core';
+import { MetaMask } from '@web3-react/metamask';
+import { WalletConnect } from '@web3-react/walletconnect';
 import TxProvider from 'provider/TxProvider';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import * as gtag from '../lib/gtag';
 import dynamic from 'next/dynamic';
+import { metaMask, hooks as metaMaskHooks } from 'connector/metaMask';
+import {
+  walletConnect,
+  hooks as walletConnectHooks,
+} from 'connector/walletConnect';
 
 const Layout = dynamic(() => import('components/Layout'));
+
+const connectors: [MetaMask | WalletConnect, Web3ReactHooks][] = [
+  [metaMask, metaMaskHooks],
+  [walletConnect, walletConnectHooks],
+];
 
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -29,7 +40,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [router.events]);
 
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
+    <Web3ReactProvider connectors={connectors}>
       <TxProvider>
         <LanguageProvider>
           <Layout>
