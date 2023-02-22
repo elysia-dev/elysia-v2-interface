2c4ef0e4877c1e5f06510d2e085e63b7bc616fbc
kyoungwan.chae <kyngwan@gmail.com>
2023-02-22 16:39:39 +0900

    Add coinbase wallet

---
+++ components/Modals/SelectWalletModal/InjectedWalletButton/CoinbaseWalletButton.tsx
@@ -0,0 +1,52 @@
+import { useWeb3React } from '@web3-react/core';
+import injectedConnector from 'core/connectors/injectedConnector';
+import Wallet from 'enums/Wallet';
+import { setWalletConnect } from 'utils/connectWallet';
+import CoinbaseSVG from 'assets/images/coinbase.svg';
+import Image from 'next/image';
+import { SelectWalletModalContentButton } from '../';
+import { activateInjectedProvider } from '.';
+
+type Props = {
+  closeModal: () => void;
+};
+const CoinbaseWalletButton: React.FC<Props> = ({ closeModal }) => {
+  const { activate } = useWeb3React();
+
+  const isCoinbasewalletInstalled =
+    window.ethereum?.isCoinbaseWallet ||
+    window.ethereum?.providers.find(
+      ({ isCoinbaseWallet }: { isCoinbaseWallet: boolean }) => isCoinbaseWallet,
+    );
+
+  const description = isCoinbasewalletInstalled
+    ? 'Coinbase Wallet'
+    : 'Install Coinbase Wallet';
+
+  const handleClickButton = async () => {
+    if (!isCoinbasewalletInstalled) {
+      window.open('https://www.coinbase.com/wallet/downloads', '_blank');
+      return;
+    }
+    activateInjectedProvider('CoinBase');
+    activate(injectedConnector)
+      .then(() => {
+        setWalletConnect(Wallet.CoinbaseWallet);
+        closeModal();
+      })
+      .catch(() => {
+        console.error('Reject');
+      });
+  };
+
+  return (
+    <SelectWalletModalContentButton
+      onClick={() => {
+        handleClickButton();
+      }}>
+      <CoinbaseSVG width={28} height={27} />
+      <b>{description}</b>
+    </SelectWalletModalContentButton>
+  );
+};
+export default CoinbaseWalletButton;
--- enums/Wallet.ts
+++ enums/Wallet.ts
@@ -1,6 +1,7 @@
 enum Wallet {
   // browser wallet
   Metamask = 'Metamask',
+  CoinbaseWallet = 'CoinbaseWallet',
   WalletConnect = 'WalletConnect',
 }
 
